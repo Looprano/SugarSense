dcdc6f1b07b310a2c8d08487c40334306599e79e
Looprano <33062083+Looprano@users.noreply.github.com>
2017-10-25 09:34:11 +0200

    fixed url problem

--- cerca.php
+++ cerca.php
@@ -10,8 +10,10 @@ $count = mysqli_num_rows($query);
 if($count == 1){
 
     while ($row=mysqli_fetch_array($query))
-    {echo $row['nome']." ".$row['cognome']." ".$row['codicefisc']." ".$row['username']." ".$row['password']." ".$row['type']." "; }
-}else{
+    {
+        echo $row['nome']." ".$row['cognome']." ".$row['codicefisc']." ".$row['username']." ".$row['password']." ".$row['type']." "; }
+}else
+    {
     echo 'Username non presente';
 }
 ?>
