f45b53064357590f6e487cabcc5ffbbedf85d9ca
Looprano <33062083+Looprano@users.noreply.github.com>
2017-10-25 12:22:58 +0200

    fixed improper neutralization

--- cerca.php
+++ cerca.php
@@ -4,14 +4,20 @@
 $conn = mysqli_connect("localhost", "root", "fedeneco123", "company");
 $username = addslashes($_POST['Username']);
 
-$sql =("SELECT nome,cognome,codicefisc,username,password,type FROM login WHERE username ='$username'");
+$sql =("SELECT nome,cognome,codicefisc,username,type FROM login WHERE username ='$username'");
 $query = mysqli_query($conn,$sql);
 $count = mysqli_num_rows($query);
 if($count == 1){
 
     while ($row=mysqli_fetch_array($query))
     {
-        echo $row['nome']." ".$row['cognome']." ".$row['codicefisc']." ".$row['username']." ".$row['password']." ".$row['type']." "; }
+        $nome = $row['nome'];
+        $cognome = $row['cognome'];
+        $codicef = $row['codicefisc'];
+        $user = $row['username'];
+        $tipo = $row['type'];
+
+        echo $nome." ".$cognome." ".$codicef." ".$user." ".$tipo." "; }
 }else
     {
     echo 'Username non presente';
