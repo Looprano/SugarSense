734747b3bbf24271f39a0950a7daa9cdf95214d0
Looprano <33062083+Looprano@users.noreply.github.com>
2017-10-31 11:57:01 +0100

    fixed securety problem

--- aggiungi.php
+++ aggiungi.php
@@ -7,11 +7,13 @@ $cf= addslashes($_POST['Codice_Fiscale']);
 $password = addslashes($_POST['Password']);
 $passmd5=md5($password);
 
-$sql =("SELECT username FROM login WHERE username ='$username'");
+$sql =sprintf("SELECT username FROM login WHERE username ='%s'",$username);
 $query = mysqli_query($conn,$sql);
 $count = mysqli_num_rows($query);
 if($count === 0){
-    $sql1 =("INSERT INTO login(nome,cognome,codicefisc,username,password,type) VALUE('$nome','$cognome','$cf','$username','$passmd5','user')");
+
+    $sql1 =sprintf("INSERT INTO login(nome,cognome,codicefisc,username,password,type) VALUE('%s','%s','%s','%s','%s','%s')",
+        $nome,$cognome,$cf,$username,$passmd5,'user');
 
     if(isset($conn) && isset($sql1))
         $query1 = mysqli_query($conn,$sql1);
