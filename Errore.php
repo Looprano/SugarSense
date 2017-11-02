cc1920b3dcf6b923831c9abde86a69187d6c0ea4
eladi96 <e.ladisa@studenti.uniba.it>
2017-11-02 10:28:39 +0100

    aggiunta messaggi errori

---
+++ Errore.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Francesca
+ * Date: 02/11/17
+ * Time: 10:19
+ */
+
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SugarSense</title>
+    <style type="text/css">
+        @import url("theme.css");
+    </style>
+</head>
+
+
+<body background="IoT.jpg">
+
+<h2>SUGARSENSE</h2>
+<h3>Amministratore</h3>
+
+<div align="center" class="errore">
+
+    <h3>Nessun Elemento Trovato</h3>
+
+
+    <footer>
+        <input type="button" class="logout" onclick="location.href='index.php'">
+        <input type="button" class="home" onclick="location.href='Utente_menu.php'">
+    </footer>
+
+</div>
+</body>
+</html>
