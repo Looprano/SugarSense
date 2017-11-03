bc9387a076c6ff61ce3d8b7822e5cc7cd2fdd366
eladi96 <e.ladisa2@studenti.uniba.it>
2017-11-03 15:00:56 +0100

    Few fixes

--- esporta_dati.php
+++ esporta_dati.php
@@ -13,8 +13,8 @@ $count = mysqli_num_rows($query);
 
 if ($count !== 0) {
 
-    htmlspecialchars($intestazione = array('ID Impianto', 'ID Sensore', 'Rilevazione', 'Descrizione'));
-    fputcsv($output, htmlspecialchars_decode($intestazione));
+    $intestazione = array('ID Impianto', 'ID Sensore', 'Rilevazione', 'Descrizione');
+    fputcsv($output, $intestazione);
     while ($row = mysqli_fetch_assoc($query)) {
         fputcsv($output, $row);
     }
